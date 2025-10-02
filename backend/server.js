830a86f6b68311a3ab303f2aeb790ce9574f1c15
Akash Kumar Singh <akkisingh952380@gmail.com>
2025-10-03 03:01:41 +0530

    booksand its model

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,8 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
-app.get("/api/test", protect, (req, res) => {
-  res.json({ message: "You are authorized!", userId: req.user.id });
-});
+const bookRoutes = require("./routes/bookRoutes");
+app.use("/api/books", bookRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
